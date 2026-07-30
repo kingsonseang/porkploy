4d3f2972362878c01712107ac4f1f70f63345992
kingsonseang <kingsonseang@gmail.com>
2026-07-30 12:15:04 +0100

    refactor(deps): switch to bun for importing env
    update `process.env` to use `Bun.env` downsides is running bun with `--bun` on every command

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? "",
+    url: Bun.env.DATABASE_URL ?? "",
   },
   dialect: "postgresql",
   out: "./drizzle",
